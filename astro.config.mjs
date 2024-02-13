ab8d3fe64e33f40affd40af9358489ef0ab45110
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-13 09:29:41 +0100

    Add @vercel/analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+})
