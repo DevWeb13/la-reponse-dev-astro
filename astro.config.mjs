8b571b5aff593f2326ae7faf4158b6b20250725d
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-25 11:13:32 +0200

    Settings script in head, Add is:inline params

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,26 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import sitemap from '@astrojs/sitemap';
-import partytown from '@astrojs/partytown';
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
-  site: 'https://www.lareponsedev.com',
-  integrations: [sitemap(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), tailwind()]
-});
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    site: 'https://www.lareponsedev.com',
+    integrations: [
+        sitemap(),
+        partytown({
+            config: {
+                forward: ['dataLayer.push'],
+            },
+        }),
+        tailwind(),
+    ],
+})
